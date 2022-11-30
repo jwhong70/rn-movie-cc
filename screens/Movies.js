c42cd628821b2a01773023b688c4f1f5b1dbbb82
[jwhong70] <[jwhong70@gmail.com]>
2022-11-30 19:34:41 +0900

    #2-1 Swiper Slider

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,9 +1,30 @@
 import React from "react";
 import styled from "styled-components/native";
+import Swiper from "react-native-swiper";
+import { Dimensions, View } from "react-native";
 
 const Container = styled.ScrollView``;
 
 const Movies = () => {
-  return null;
+  const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+  return (
+    <Container>
+      <Swiper
+        horizontal
+        loop
+        autoplay
+        autoplayTimeout={3.5}
+        showsButtons={false}
+        showsPagination={false}
+        containerStyle={{
+          marginBottom: 40,
+          width: "100%",
+          height: SCREEN_HEIGHT / 4,
+        }}
+      >
+        <View></View>
+      </Swiper>
+    </Container>
+  );
 };
 export default Movies;

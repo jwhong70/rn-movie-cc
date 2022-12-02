650851f5e7b6c24210646aa464ee85150a83e44b
[jwhong70] <[jwhong70@gmail.com]>
2022-12-02 13:46:41 +0900

    #3-1 React-query

--- screens/Tv.js
+++ screens/Tv.js
@@ -1,7 +1,30 @@
 import React from "react";
 import styled from "styled-components/native";
+import Loader from "./../components/Loader";
+import { ScrollView } from "react-native";
+import { useQueryClient } from "@tanstack/react-query";
 
 const Tv = () => {
-  return null;
+  const { isLoading: trendingLoading, data: trendingData } = useQuery(
+    ["tv", "trending"],
+    tvApi.trending
+  );
+  const { isLoading: todayLoading, data: todayData } = useQuery(
+    ["tv", "today"],
+    tvApi.airingToday
+  );
+  const { isLoading: topLoading, data: topData } = useQuery(
+    ["tv", "top"],
+    tvApi.topRated
+  );
+  const loading = trendingLoading || todayLoading || topLoading;
+  const queryClient = useQueryClient();
+  const [refreshing, setRefreshing] = useState(false);
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await queryClient.refetchQueries(["tv"]);
+    setRefreshing(false);
+  };
+  return loading ? <Loader /> : <ScrollView></ScrollView>;
 };
 export default Tv;

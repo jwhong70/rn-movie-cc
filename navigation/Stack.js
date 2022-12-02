110242c9f8a0faf3e8591d32d1322d2c3b31e89e
[jwhong70] <[jwhong70@gmail.com]>
2022-12-02 18:14:20 +0900

    #5-1 Detail Screen Layout

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,11 +1,20 @@
 import React from "react";
 import styled from "styled-components/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useColorScheme } from "react-native";
+import { BLACK_COLOR } from "./../colors";
 
 const Stack = () => {
   const Stack = createNativeStackNavigator();
+  const isDark = useColorScheme() === "dark";
   return (
-    <Stack.Navigator screenOptions={{ headerBackTitleVisible: false }}>
+    <Stack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: false,
+        headerStyle: { backgroundColor: isDark ? BLACK_COLOR : "white" },
+        headerTitleStyle: { color: isDark ? "white" : BLACK_COLOR },
+      }}
+    >
       <Stack.Screen name="Detail" component={Detail} />
     </Stack.Navigator>
   );
--- screens/Detail.js
+++ screens/Detail.js
@@ -1,7 +1,11 @@
 import React from "react";
 import styled from "styled-components/native";
 
+const Container = styled.ScrollView`
+  background-color: ${(props) => props.theme.mainBgColor};
+`;
+
 const Detail = () => {
-  return null;
+  return <Container></Container>;
 };
 export default Detail;
